bbf66464da0aa61ef9a888e2b9d96da5f69b5804
Tyler <tyler.tomczyk@gmail.com>
2019-03-05 18:12:45 +0000

    step 1

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // Arrays to keep track of each task's state
 const taskTitles = [];
 const taskComplete = [];
+const taskDescriptions = [];
 
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
-function newTask(title) {
+function newTask(title, description) {
   taskTitles.push(title);
+  taskDescriptions.push(description);
   taskComplete.push(false);
 }
 
